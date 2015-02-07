88204fc87185ab90f12e71eaaf75a99ab05eadcb
Nick <haibo.nick.jin@gmail.com>
2015-02-07 15:59:59 +0200

    multi_accuracy

--- ex1/multi_classifier_accuracy.m
+++ ex1/multi_classifier_accuracy.m
@@ -1,5 +1,5 @@
 function accuracy=binary_classifier_accuracy(theta, X,y)
-  [~,labels] = max(theta'*X, [], 1);
+  [values,labels] = max(theta'*X, [], 1);
 
   correct=sum(y == labels);
   accuracy = correct / length(y);
